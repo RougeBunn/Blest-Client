04ff866bce1ab8d1058936afe4fdd7ebf5273b39
Zack Bunn <zrb6578@gmail.com>
2019-08-12 22:09:53 -0500

    trying someething new with header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -11,6 +11,12 @@ export default class Header extends Component {
     toggleLoggedIn: () => {}
   };
 
+  componentDidMount() {
+    // const { userId } = this.props.match.params;
+    const userId = this.props.match.params.userId;
+    this.props.fetchBlessings(userId);
+  }
+
   handleLogoutClick = () => {
     TokenService.clearAuthToken();
     console.log('loggedin props', this.props);
@@ -18,55 +24,29 @@ export default class Header extends Component {
     console.log('loggedin State', this.props.state);
   };
 
-  // renderLogoutLink() {
-  //   return (
-  //     <div className="Header__logged-in">
-  //       <Link to={`/userpage/${this.context.userId}`}>Your List</Link>
-  //       <Link onClick={this.handleLogoutClick} to="/">
-  //         Logout
-  //       </Link>
-  //     </div>
-  //   );
-  // }
-
-  // renderLoginLink() {
-  //   return (
-  //     <div className="Header__not-logged-in">
-  //       <Link to="/login">Log in</Link>
-  //       <Link to="/register">Sign Up</Link>
-  //     </div>
-  //   );
-  // }
-
-  render() {
-    // return (
-    //   <AppContext.Consumer>
-    //     {value => (
-    //       <nav className="Header">
-    //         <h1>
-    //           <Link to="/">BLest</Link>
-    //         </h1>
-    //         {value.state.isLoggedIn
-    //           ? this.renderLogoutLink()
-    //           : this.renderLoginLink()}
-    //       </nav>
-    //     )}
-    //   </AppContext.Consumer>
-    const logOutLink = (
+  renderLogoutLink() {
+    return (
       <div className="Header__logged-in">
-        <Link to={`/userpage/${this.context.userId}`}>Your List</Link>
+        <Link to={`/userpage/${this.props.match.params.userId}`}>
+          Your List
+        </Link>
         <Link onClick={this.handleLogoutClick} to="/">
           Logout
         </Link>
       </div>
     );
-    const logInLink = (
+  }
+
+  renderLoginLink() {
+    return (
       <div className="Header__not-logged-in">
         <Link to="/login">Log in</Link>
         <Link to="/register">Sign Up</Link>
       </div>
     );
+  }
 
+  render() {
     return (
       <AppContext.Consumer>
         {value => (
@@ -74,10 +54,38 @@ export default class Header extends Component {
             <h1>
               <Link to="/">BLest</Link>
             </h1>
-            {value.state.isLoggedIn ? logOutLink : logInLink}
+            {value.state.isLoggedIn
+              ? this.renderLogoutLink()
+              : this.renderLoginLink()}
           </nav>
         )}
       </AppContext.Consumer>
+      // const logOutLink = (
+      //   <div className="Header__logged-in">
+      //     <Link to={`/userpage/${this.props.match.params.userId}`}>Your List</Link>
+      //     <Link onClick={this.handleLogoutClick} to="/">
+      //       Logout
+      //     </Link>
+      //   </div>
+      // );
+      // const logInLink = (
+      //   <div className="Header__not-logged-in">
+      //     <Link to="/login">Log in</Link>
+      //     <Link to="/register">Sign Up</Link>
+      //   </div>
+      // );
+
+      // return (
+      //   <AppContext.Consumer>
+      //     {value => (
+      //       <nav className="Header">
+      //         <h1>
+      //           <Link to="/">BLest</Link>
+      //         </h1>
+      //         {value.state.isLoggedIn ? logOutLink : logInLink}
+      //       </nav>
+      //     )}
+      //   </AppContext.Consumer>
     );
   }
 }
